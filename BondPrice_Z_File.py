29a97c8c1f3c74b707d3bb34f68803c8ac2a6675
Linke-Yu-357 <ly2691@columbia.edu>
2025-10-05 19:20:51 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,16 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    c = face * couponRate
+    price = 0.0
+    n = len(times)
+    for k, (t, y) in enumerate(zip(times, yc), start=1):
+        cf = c if k < n else (c + face)       
+        price += cf / (1 + y) ** t        
+    return price
+yc = [0.010, 0.015, 0.020, 0.025, 0.030]
+times = [1, 1.5, 3, 4, 7]
+face = 2_000_000
+couponRate = 0.04
+p = getBondPrice_Z(face, couponRate, times, yc)
+print(p)
